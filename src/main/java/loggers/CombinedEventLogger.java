080c067a1b29f7634cbd61877ca21e342deeed9f
igor <igorer27@yandex.ru>
2018-06-07 09:53:35 +0300

    Lecture 9:Fixes

--- src/main/java/loggers/CombinedEventLogger.java
+++ src/main/java/loggers/CombinedEventLogger.java
@@ -3,15 +3,11 @@ package loggers;
 import app.Event;
 import interfaces.EventLogger;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Data
-@Component(value = "combinedEventLogger")
 public class CombinedEventLogger implements EventLogger {
-    @Resource
     private List<EventLogger> loggers;
 
     @Override
